2123b932d641c07a777129a24b44ef5351660e28
adriano.bonin <adriano.bonin@outlook.com>
2021-03-31 17:03:10 -0300

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/HyperCalc.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HyperCalc.dir/main.cpp.obj"
+  "CMakeFiles/HyperCalc.dir/mymath.cpp.obj"
+  "HyperCalc.exe"
+  "HyperCalc.exe.manifest"
+  "HyperCalc.pdb"
+  "libHyperCalc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HyperCalc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
